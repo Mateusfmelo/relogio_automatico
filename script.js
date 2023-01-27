c1dc4532a1cc3f7a48515b8d860b67f19cdee6a0
mateusfmelo <mateusmelo51@gmail.com>
2023-01-27 18:55:10 -0300

    Versão 2.0

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
 
-    var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas!`
+    var horas = new Date()
+    var hora = horas.getHours()
+
+    var minutos = new Date()
+    var minuto = minutos.getMinutes()
+
+    msg.innerHTML = `Agora são ${hora} hora(s) e ${minuto} minuto(s)!`
 
     if (hora >= 0 && hora < 12) {
         day.innerHTML = 'Bom dia!'
         img.src = 'manha.png'
         document.body.style.background = '#F3A68C'
-    } else if (hora >= 12 && hora <= 18) {
+    } else if (hora >= 12 && hora < 18) {
         day.innerHTML = 'Boa tarde!'
         img.src = 'tarde.png'
         document.body.style.background = '#FD9F4E'
